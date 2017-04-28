3d7cb3eac5411ae3707a3c66f6c15c32be53c961
DingdingKim <twlovesh89@gmail.com>
2017-04-28 23:04:03 +0900

    필요없는 코드 정리

--- app/src/main/java/com/dingding/chuchu/ActMain.java
+++ app/src/main/java/com/dingding/chuchu/ActMain.java
@@ -1,10 +1,7 @@
 package com.dingding.chuchu;
 
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.luolc.emojirain.EmojiRainLayout;
@@ -14,8 +11,6 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class ActMain extends Activity {
-    @Bind(R.id.main_img_chu)
-    ImageView img_chu;
     @Bind(R.id.main_tv_chu_name)
     TextView tv_chu_name;
     @Bind(R.id.main_lay_root)
@@ -27,7 +22,7 @@ public class ActMain extends Activity {
         setContentView(R.layout.act_main);
         ButterKnife.bind(this);
 
-        // add emoji sources
+        //떨어지는 이미지 추가
         lay_root.addEmoji(R.drawable.melon_emoji);
     }
 
@@ -38,9 +33,5 @@ public class ActMain extends Activity {
         if(BuildConfig.FLAVOR.equals("melon")){
             lay_root.startDropping();
         }
-//필요하다면 패키지명으로 구분해도될듯
-//        if(getPackageName().equals("com.dingding.chuchu_strawberry")){
-//            lay_root.startDropping();
-//        }
     }
 }
